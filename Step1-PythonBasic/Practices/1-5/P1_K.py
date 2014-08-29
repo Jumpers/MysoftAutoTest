9e630752ac8f28a1300984fac252fd8731d8d0ad
polestark <keven.zhu@gmail.com>
2014-08-29 18:31:05 +0800

    add practices 1 by Kai

---
+++ Step1-PythonBasic/Practices/1-5/P1_K.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-  
+print "helloworld"
+print "hello again"
+print "I like typing this"
+print "this is fun."
+print 'Yay!Printing'
+print "I'd much rather your 'not'."
+print 'I "said" do not touch this.'
+
+# 加分练习
+print "add more than one line"
+
+#add comment by add "#"
+# print "game player"
