033dd00161ff76b722900f70b923b0dfc7eb2b34
polestark <keven.zhu@gmail.com>
2014-08-30 10:15:16 +0800

    add practice 2

---
+++ Step1-PythonBasic/Practices/1-5/P2_K.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# as a comment, you can see anything. include "中文字符"
+print "中文字符" # this information will be ignored
+# print "hello this comment"
+print "this will be run"
+#
+#
+#
+#
+#
+#
