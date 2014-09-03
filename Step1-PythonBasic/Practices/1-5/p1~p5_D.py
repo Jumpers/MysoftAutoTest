4d49651e8f5568e5ba0bbbd6e2ce26c8ea24ec75
dengt <dengt@wh-dengt.mysoft.com.cn>
2014-09-03 09:12:28 +0800

    DT

--- Step1-PythonBasic/Practices/1-5/p1~p5_D.py
+++ Step1-PythonBasic/Practices/1-5/p1~p5_D.py
@@ -71,25 +71,25 @@ print "We have",passengers,"to carpool today."
 print "We need to put about",average_passengers_per_car,"in each car."
 #car_pool_capacity未定义
 #加分练习
-print "enter the number of cars"
-cars=raw_input()
-print "enter the number of space_in_a_car"
-space_in_a_car=raw_input()
-print "enter the number of drivers"
-drivers=raw_input()
-print "enter the number of passengers"
-passengers=raw_input()
-cars_not_driven=cars-drivers
-cars_driven=drivers
-carpool_capacity=cars_driven*space_in_a_car
-average_passengers_per_car=passengers/cars_driven
+#print "enter the number of cars"
+#cars=raw_input()
+#print "enter the number of space_in_a_car"
+#space_in_a_car=raw_input()
+#print "enter the number of drivers"
+#drivers=raw_input()
+#print "enter the number of passengers"
+#passengers=raw_input()
+#cars_not_driven=cars-drivers
+#cars_driven=drivers
+#carpool_capacity=cars_driven*space_in_a_car
+#average_passengers_per_car=passengers/cars_driven
 
-print "There are",cars,"cars available."
-print "There are only",drivers,"drivers available."
-print "There will be",cars_not_driven,"empty cars today."
-print "We can transport",carpool_capacity,"people today."
-print "We have",passengers,"to carpool today."
-print "We need to put about",average_passengers_per_car,"in each car."
+#print "There are",cars,"cars available."
+#print "There are only",drivers,"drivers available."
+#print "There will be",cars_not_driven,"empty cars today."
+#print "We can transport",carpool_capacity,"people today."
+#print "We have",passengers,"to carpool today."
+#print "We need to put about",average_passengers_per_car,"in each car."
 #raw_input()捕获的是字符串
 
 
