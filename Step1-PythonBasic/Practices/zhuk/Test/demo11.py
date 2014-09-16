2b0fcd9f3eab46a1df053fd3d65d6a002a9001f0
polestark <keven.zhu@gmail.com>
2014-09-16 22:34:02 +0800

    test11

---
+++ Step1-PythonBasic/Practices/zhuk/Test/demo11.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+print "how old are your?",
+age = raw_input()
+print "how tall are you?",
+height = raw_input()
+print "how much do your weight?",
+weight = raw_input()
+
+print "so, you're %r old, %r tall, %r weight" %(age,height,weight)
+
+print "how old are your?",
+age = input()
+print "how tall are you?",
+height = input()
+print "how much do your weight?",
+weight = input()
+
+print "so, you're %r old, %r tall, %r weight" %(age,height,weight)
+
+
+'''comment'''
+print "the different of input and raw_input"
+x = input(raw_input())
+y = input()
+print "%r x, %r y" %(x,y)
+
+
