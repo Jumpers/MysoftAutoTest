b56f87b0e82f01a101503295ad658e4a6cc0e79b
dengt <dengt@wh-dengt.mysoft.com.cn>
2014-09-16 16:49:12 +0800

    dt

---
+++ Step1-PythonBasic/Practices/dengt/p11_DT.py
@@ -0,0 +1,31 @@
+#-*- coding:utf-8 -*-
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weight?",
+# weight = raw_input()
+# 
+# print "So,you're %r old,%r tall and %r heavy." %(age,height,weight)
+
+#练习12
+# raw_input_A = input("raw_input:")
+# print raw_input_A
+
+# age = raw_input("How old are you? ")
+# height = raw_input("How tall are you?　")
+# weight = raw_input("How much do you weigh? ")
+# 
+# print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
+
+#练习13
+# from sys import argv
+# 
+# script, first, second, third = argv
+# 
+# print "The script is called:", script
+# print "Your first variable is:", first
+# print "Your second variable is:", second
+# print "Your third variable is:", third
+raw_input(x)='?'
+
