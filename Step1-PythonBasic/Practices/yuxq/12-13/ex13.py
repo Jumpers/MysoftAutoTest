e2595568774f940c2c20bb62b24b1cce8f383c1a
yuxq01 <780359798@qq.com>
2014-09-23 17:19:06 +0800

    作业提交

---
+++ Step1-PythonBasic/Practices/yuxq/12-13/ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+script,first,second,third=argv
+print "The script id called:",script
+print "Your first variable is:",first
+print "Your second variable is:",second
+print "Your third variable is:",third
+a=type(script)
+b=type(first)
+c=type(second)
+d=type(third)
+print a,b,c,d
+
