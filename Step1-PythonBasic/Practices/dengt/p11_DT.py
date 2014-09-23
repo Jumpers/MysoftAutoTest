8d615ba5d33642d4d947c25f5b11575e4a95ea68
dengt <dengt@wh-dengt.mysoft.com.cn>
2014-09-23 17:55:38 +0800

    dt

--- Step1-PythonBasic/Practices/dengt/p11_DT.py
+++ Step1-PythonBasic/Practices/dengt/p11_DT.py
@@ -5,27 +5,41 @@
 # height = raw_input()
 # print "How much do you weight?",
 # weight = raw_input()
-# 
+#  
 # print "So,you're %r old,%r tall and %r heavy." %(age,height,weight)
 
+#input()传入的是一个可接受的合法的python表达式
+#raw_input()传入的是字符串
+
+# hehe = raw_input("comeon baby!")
+# 
+# print hehe
+
+# A=input("Please enter A")
+# B=raw_input("Please enter B")
+# print type(A),type(B)
+# print A,B
+
+
+
 #练习12
-# raw_input_A = input("raw_input:")
-# print raw_input_A
+raw_input_A = input("raw_input:")
+print raw_input_A
 
-# age = raw_input("How old are you? ")
-# height = raw_input("How tall are you?　")
-# weight = raw_input("How much do you weigh? ")
-# 
-# print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
-
-#练习13
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you?　")
+weight = raw_input("How much do you weigh? ")
+ 
+print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
+ 
+# 练习13
 # from sys import argv
-# 
+#   
 # script, first, second, third = argv
-# 
+#   
 # print "The script is called:", script
 # print "Your first variable is:", first
 # print "Your second variable is:", second
 # print "Your third variable is:", third
-raw_input(x)='?'
+
 
