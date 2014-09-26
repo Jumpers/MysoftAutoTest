98ee61d11757f82422d9df3eb783d528096c3b6d
dengt <dengt@wh-dengt.mysoft.com.cn>
2014-09-26 09:25:15 +0800

    dt

--- Step1-PythonBasic/Practices/dengt/p11&12&13_DT.py
+++ Step1-PythonBasic/Practices/dengt/p11&12&13_DT.py
@@ -15,23 +15,23 @@
 # 
 # print hehe
 
-# A=input("Please enter A")
-# B=raw_input("Please enter B")
-# print type(A),type(B)
-# print A,B
+A=input("Please enter A")
+B=raw_input("Please enter B")
+print type(A),type(B)
+print A,B
 
 
 
 #练习12
 raw_input_A = input("raw_input:")
 print raw_input_A
-
+ 
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you?　")
 weight = raw_input("How much do you weigh? ")
- 
+  
 print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
- 
+  
 # 练习13
 # from sys import argv
 #   
@@ -41,5 +41,6 @@ print "So, you're %r old, %r tall and %r heavy." % (age,height,weight)
 # print "Your first variable is:", first
 # print "Your second variable is:", second
 # print "Your third variable is:", third
+
 
 
