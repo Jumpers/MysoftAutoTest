8d452e1500bccf006519655a5bdbfda097d3bff9
dengt <dengt@wh-dengt.mysoft.com.cn>
2014-11-11 13:25:40 +0800

    dt

--- Step1-PythonBasic/Practices/dengt/p20_DT.py
+++ Step1-PythonBasic/Practices/dengt/p20_DT.py
@@ -3,32 +3,36 @@ from sys import argv
 
 script, input_file = argv
 
-def print_all(f):
-    print f.read()
-    
-def rewind(f):
-    f.seek(0)#位移为0
-    
-def print_a_line(line_count, f):
-    print line_count, f.readline()
-    
+# def print_all(f):
+#     print f.read()
+#     
+# def rewind(f):
+#     f.seek(0)#位移为0
+#     
+# def print_a_line(line_count, f):
+#     print line_count, f.readline()
+#     
+# current_file = open(input_file)
+# 
+# print "First let's print the whole file:\n"
+# 
+# print_all(current_file)
+# 
+# print "Now let's rewind, kind of like a tape."
+# 
+# rewind(current_file)
+# 
+# print "Let's print three lines:"
+# 
+# current_line = 1
+# print_a_line(current_line, current_file)
+#  
+# current_line = current_line + 1
+# print_a_line(current_line,current_file)
+#  
+# current_line = current_line + 1
+# print_a_line(current_line,current_file)
 current_file = open(input_file)
-
-print "First let's print the whole file:\n"
-
-print_all(current_file)
-
-print "Now let's rewind, kind of like a tape."
-
-rewind(current_file)
-
-print "Let's print three lines:"
-
-current_line = 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line(current_line,current_file)
-
-current_line = current_line + 1
-print_a_line(current_line,current_file)
+current_file.readline()
+current_file.readline()
+current_file.readline()
